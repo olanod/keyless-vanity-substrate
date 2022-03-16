73edd0070212c7dca6de07130efd1acfc7d151c8
Daniel Olano <me@olanod.com>
2022-03-17 00:37:45 +0100

    Vanity finder for Polkadot addresses

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,49 @@
+//#![no_std]
+
+use base58::*;
+use core::iter::repeat;
+
+pub fn vanity(input: &str, fill_c: char) -> Option<String> {
+    let mut chars = input.chars().map(replace_invalid);
+    let s = repeat(fill_c).take(46).map(|c| chars.next().unwrap_or(c));
+    let s: String = ['1', '1'].into_iter().chain(s).collect();
+
+    let mut bytes = s.as_str().from_base58().ok()?;
+    let len = bytes.len();
+    let [h1, h2]: [u8; 2] = {
+        let h = ss58hash(&bytes[1..len - 2]);
+        h.as_bytes()[..2].try_into().unwrap()
+    };
+    // println!("{} -> {:?}", bytes.len(), bytes);
+    bytes[len - 2] = h1;
+    bytes[len - 1] = h2;
+    Some(bytes[1..].to_base58())
+}
+
+const fn replace_invalid(c: char) -> char {
+    match c {
+        'O' => 'o',
+        'I' => 'i',
+        'l' => 'L',
+        _ => c,
+    }
+}
+
+fn ss58hash(data: &[u8]) -> blake2_rfc::blake2b::Blake2bResult {
+    const PREFIX: &[u8] = b"SS58PRE";
+    let mut context = blake2_rfc::blake2b::Blake2b::new(64);
+    context.update(PREFIX);
+    context.update(data);
+    context.finalize()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn vanity_address() {
+        let address = vanity("HelloWorld", '1').expect("valid address");
+        assert_eq!(address, "1HeLLoWorLd1111111111111111111111111111111112kn");
     }
 }
